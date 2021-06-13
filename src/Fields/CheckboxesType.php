73b792238dea144dc7177229373ce822930b7cf5
Rudie Dirkx <168024+rudiedirkx@users.noreply.github.com>
2021-06-13 17:04:21 +0200

    +CheckboxesType default value from BelongsToMany relationship

--- src/Fields/CheckboxesType.php
+++ src/Fields/CheckboxesType.php
@@ -2,9 +2,18 @@
 
 namespace rdx\LfbExtras\Fields;
 
+use Illuminate\Database\Eloquent\Collection;
 use Kris\LaravelFormBuilder\Fields\SelectType;
 
 class CheckboxesType extends SelectType {
+
+	public function setValue($value) {
+		if ($value instanceof Collection) {
+			$value = $value->modelKeys();
+		}
+
+		parent::setValue($value);
+	}
 
 	protected function getTemplate() {
 		return 'laravel-form-builder::checkboxes';
